f4b1890c615bb4d6f76ba8b7699ac95858fc49a9
helin <lin.he@datamind.tech>
2024-11-01 16:54:13 +0800

    change version

--- src/version.ts
+++ src/version.ts
@@ -1,7 +1,1 @@
-/*
- * @Description: 
- * @Author: czw
- * @Date: 2022-01-24 19:06:34
- * @FilePath: \vscode-background-cover-online\src\version.ts
- */
-export default '2.0.5';
+export default '2.0.6';
